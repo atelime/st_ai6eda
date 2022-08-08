614dfeac65e792a864828eeee1cc7e671181df29
atelime <atelierlime@gmail.com>
2022-08-08 22:44:45 +0900

    Update and rename boston-house.py to houseprice.py

---
+++ pages/houseprice.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+import shap
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.ensemble import RandomForestRegressor
+
+st.write("""
+# Boston House Price Prediction App
+
+This app predicts the **Boston House Price**!
+""")
+st.write('---')
+
+# Loads the House Price Dataset
+house = datasets.fetch_california_housing()
+X = pd.DataFrame(house.data, columns=house.feature_names)
+Y = pd.DataFrame(house.target, columns=["MEDV"]
+
+# Sidebar
+# Header of Specify Input Parameters
+st.sidebar.header('Specify Input Parameters')
+
+def user_input_features():
+    data = {}
+    for feature in house.feature_names:
+        data[feature] = st.sidebar.slider(feature, X[feature].min(), X[feature].max(), X[feature].mean())
+        features = pd.DataFrame(data, index=[0])
+        features = pd.DataFrame(data, index=[0])
+        return features
+
+df = user_input_features()
+
+# Main Panel
+
+# Print specified input parameters
+st.header('Specified Input parameters')
+st.write(df)
+st.write('---')
+
+# Build Regression Model
+model = RandomForestRegressor()
+model.fit(X, Y)
+# Apply Model to Make Prediction
+prediction = model.predict(df)
+
+st.header('Prediction of MEDV')
+st.write(prediction)
+st.write('---')
+
+# Explaining the model's predictions using SHAP values
+# https://github.com/slundberg/shap
+explainer = shap.TreeExplainer(model)
+shap_values = explainer.shap_values(X)
+
+st.header('Feature Importance')
+plt.title('Feature importance based on SHAP values')
+shap.summary_plot(shap_values, X)
+st.pyplot(bbox_inches='tight')
+st.write('---')
+
+plt.title('Feature importance based on SHAP values (Bar)')
+shap.summary_plot(shap_values, X, plot_type="bar")
+st.pyplot(bbox_inches='tight')
