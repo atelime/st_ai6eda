5f3cd0a25b82e785c5c75bdfdfc68d2ecefd7041
atelime <atelierlime@gmail.com>
2022-08-09 00:41:34 +0900

    Update houseprice.py

--- pages/houseprice.py
+++ pages/houseprice.py
@@ -44,3 +44,18 @@ prediction = model.predict(df)
 st.header(f'Prediction of {house.target_names}')
 st.write(prediction)
 st.write('---')
+
+# Explaining the model's predictions using SHAP values
+# https://github.com/slundberg/shap
+# explainer = shap.TreeExplainer(model)
+# shap_values = explainer.shap_values(X)
+
+# st.header('Feature Importance')
+# plt.title('Feature importance based on SHAP values')
+# shap.summary_plot(shap_values, X)
+# st.pyplot(bbox_inches='tight')
+# st.write('---')
+
+# plt.title('Feature importance based on SHAP values (Bar)')
+# shap.summary_plot(shap_values, X, plot_type="bar")
+# st.pyplot(bbox_inches='tight')
